ab16e39620bad9055e5580e1070f18f4d88adce1
yifanliuu <kokonoseren@163.com>
2020-06-02 12:46:24 +0800

    add: config.py

---
+++ src/config.py
@@ -0,0 +1,6 @@
+# configs
+TRAIN_AUTHOR_PATH = '../dataset/train_author.json'
+TRAIN_PUB_PATH = '../dataset/train_pub.json'
+
+VAL_AUTHOR_PATH = '../dataset/valid/sna_valid_author_raw.json'
+VAL_PUB_PATH = '../dataset/valid/sna_valid_pub.json'
