1946891f68fd809d243b185f5506003988efdcfa
yifanliuu <kokonoseren@163.com>
2020-06-02 16:32:40 +0800

    add: generate candidate sets

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,2 +1,49 @@
 #
 # TODO: load the file and convert to torch tensors
+
+# ---------------load authors.json to generate ground truth and all papaers--------------
+from utils import load_json
+import config as cfg
+
+
+def generateCandidateSets(mode):
+    '''
+    mode: 'train' or 'val'
+    '''
+    authors = None
+    if mode == 'train':
+        authors = load_json(cfg.TRAIN_AUTHOR_PATH)
+    elif mode == 'val':
+        authors = load_json(cfg.VAL_AUTHOR_PATH)
+    else:
+        raise Exception("mode should be 'train' or 'val'\n")
+
+    pubs_by_name = {}
+    labels_by_name = {}
+    # {name: pubs}
+
+    for n, name in enumerate(authors):
+        i_label = 0
+        pubs = []  # all papers
+        labels = []  # ground truth
+
+        for identity in authors[name]:
+            identity_pubs = authors[name][identity]
+            for pub in identity_pubs:
+                pubs.append(pub)
+                labels.append(i_label)
+            i_label += 1
+
+        # print(n, name, len(pubs))
+        pubs_by_name[name] = pubs
+        labels_by_name[name] = labels
+
+    return labels_by_name, pubs_by_name
+
+
+if __name__ == "__main__":
+    pass
+    # ---------generateCandidateSets test------------
+    # labels_by_name, pubs_by_name,  = generateCandidateSets('train')
+    # print(labels_by_name)
+    # print(pubs_by_name)
--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,72 @@
+import config as cfg
+# -------------load data--------------
+import codecs
+import json
+from os.path import join
+import pickle
+import os
+import re
+
+################# Load and Save Data ################
+
+
+def load_json(rfpath):
+    with codecs.open(rfpath, 'r', encoding='utf-8') as rf:
+        return json.load(rf)
+
+
+def dump_json(obj, wfpath, wfname, indent=None):
+    with codecs.open(join(wfpath, wfname), 'w', encoding='utf-8') as wf:
+        json.dump(obj, wf, ensure_ascii=False, indent=indent)
+
+
+def dump_data(obj, wfpath, wfname):
+    with open(os.path.join(wfpath, wfname), 'wb') as wf:
+        pickle.dump(obj, wf)
+
+
+def load_data(rfpath):
+    with open(rfpath, 'rb') as rf:
+        return pickle.load(rf)
+
+# -------------save features----------
+#
+
 # TODO: inverted document frequency
+
+# -------------evaluate---------------
+
+
+def pairwise_evaluate(correct_labels, pred_labels):
+    TP = 0.0  # Pairs Correctly Predicted To SameAuthor
+    TP_FP = 0.0  # Total Pairs Predicted To SameAuthor
+    TP_FN = 0.0  # Total Pairs To SameAuthor
+
+    for i in range(len(correct_labels)):
+        for j in range(i + 1, len(correct_labels)):
+            if correct_labels[i] == correct_labels[j]:
+                TP_FN += 1
+            if pred_labels[i] == pred_labels[j]:
+                TP_FP += 1
+            if (correct_labels[i] == correct_labels[j]) and (pred_labels[i] == pred_labels[j]):
+                TP += 1
+
+    if TP == 0:
+        pairwise_precision = 0
+        pairwise_recall = 0
+        pairwise_f1 = 0
+    else:
+        pairwise_precision = TP / TP_FP
+        pairwise_recall = TP / TP_FN
+        pairwise_f1 = (2 * pairwise_precision * pairwise_recall) / \
+            (pairwise_precision + pairwise_recall)
+    return pairwise_precision, pairwise_recall, pairwise_f1
+
+
+if __name__ == "__main__":
+    pass
+    # --------load json test-----------
+    # pubs_train = load_json(cfg.TRAIN_PUB_PATH)
+    # pubs_val = load_json(cfg.VAL_PUB_PATH)
+    # print(pubs_train)
+    # print(pubs_val)
